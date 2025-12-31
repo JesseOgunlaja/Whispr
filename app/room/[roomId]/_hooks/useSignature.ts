a04cd2927a1383eb93881602dfb5bf7d47cd97fe
JesseOgunlaja <jesseogunlaja@gmail.com>
2025-12-31 13:01:30 +0000

    fix: ensure correct public keys are sent to server

--- app/room/[roomId]/_hooks/useSignature.ts
+++ app/room/[roomId]/_hooks/useSignature.ts
@@ -1,16 +1,19 @@
+import { usePublicKeys } from "@/app/_components/PublicKeysProvider";
 import { createSignature } from "@/lib/crypto/signing";
 import { useQuery } from "@tanstack/react-query";
 import { useRoomId } from "./useRoomId";
 
 export function useSignature() {
     const roomId = useRoomId();
+    const { signingKey } = usePublicKeys();
+
     const { data } = useQuery({
         queryKey: ["signature", roomId],
         queryFn: () => createSignature(roomId!),
         refetchInterval: 55000,
         refetchIntervalInBackground: true,
         gcTime: 0,
-        enabled: !!roomId,
+        enabled: !!roomId && !!signingKey,
     });
 
     return data;

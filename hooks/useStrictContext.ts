dc0c4727e49e6e40e5840d1b7137cab4372ebe20
JesseOgunlaja <jesseogunlaja@gmail.com>
2026-01-06 17:24:00 +0000

    styles: add empty line for readability

--- hooks/useStrictContext.ts
+++ hooks/useStrictContext.ts
@@ -3,5 +3,6 @@ import { Context, useContext } from "react";
 export function useStrictContext<T>(context: Context<T>) {
     const ctx = useContext(context);
     if (!ctx) throw new Error("Context must be used within correct provider");
+
     return ctx;
 }
